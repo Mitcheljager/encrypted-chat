c91d7568135f46338bcf4982d5e10230c5adc610
Mitchel <mitchel_jager@hotmail.com>
2019-12-12 23:57:13 +0100

    Use Webcomponent for chat items

--- app/javascript/channels/chat_channel.js
+++ app/javascript/channels/chat_channel.js
@@ -14,12 +14,10 @@ consumer.subscriptions.create({ channel: "ChatChannel", uuid: getUUID() } , {
 
   received(data) {
     const chatElement = document.querySelector("[data-role='chat-container']")
-    const textElement = data.type == "server" ? createServerItem(data) : createChatItem(data)
+    const textElement = createChatItem(data)
     chatElement.append(textElement)
 
     textElement.scrollIntoView()
-
-    if (chatElement.dataset.expiration > 0) timeOutRemoveElement(textElement, chatElement.dataset.expiration)
   }
 })
 
@@ -29,111 +27,18 @@ function getUUID() {
 }
 
 function createChatItem(data) {
-  const element = document.createElement("div")
-  element.classList.add("chat-item")
+  const element = document.createElement("chat-item")
+  element.setAttribute("username", data.username || "")
+  element.setAttribute("color", data.color)
+  element.setAttribute("timestamp", createTimestamp(data.timestamp))
+  element.setAttribute("type", data.type || "message")
+
   element.innerText = data.content
-
-  const usernameElement = document.createElement("span")
-  usernameElement.classList.add("chat-item__user")
-  usernameElement.style.color = data.color
-  usernameElement.innerText = data.username
-
-  const timeElement = createTimestamp(data.timestamp)
-
-  element.prepend(usernameElement)
-  element.append(timeElement)
-
-  return element
-}
-
-function createServerItem(data) {
-  const element = document.createElement("div")
-  element.classList.add("chat-item")
-  element.classList.add("chat-item--server")
-  element.innerText = data.content
-
-  const timeElement = createTimestamp(data.timestamp)
-
-  element.append(timeElement)
 
   return element
 }
 
 function createTimestamp(timestamp) {
   const dateTime = new Date(timestamp)
-  const timeElement = document.createElement("span")
-  timeElement.classList.add("chat-item__time")
-  timeElement.title = timestamp
-  timeElement.innerText = `${ ("0" + dateTime.getHours()).slice(-2) }:${ ("0" + dateTime.getMinutes()).slice(-2) }:${ ("0" + dateTime.getSeconds()).slice(-2) }`
-
-  return timeElement
-}
-
-function timeOutRemoveElement(element, time) {
-  const timerElement = document.createElement("div")
-  timerElement.classList.add("chat-item__timer")
-  timerElement.style.animationDuration = time + "s"
-
-  element.append(timerElement)
-
-  setTimeout(() => { element.classList.add("chat-item--is-fading-out") }, time * 1000)
-  element.addEventListener("transitionend", () => {
-    element.remove()
-  })
-}
-
-function buildChatItem() {
-  customElements.define("chat-item",
-    class ChatItem extends HTMLElement {
-      connectedCallback() {
-        this.addEventListener("click", this.removeElement)
-      }
-
-      constructor() {
-        super()
-
-        const template = document.createElement("template")
-        template.innerHTML = `
-          <div class="toast">
-            <slot></slot>
-
-            <div part="progress"></div>
-          </div>
-        `
-
-        const shadowRoot = this.attachShadow({mode: "open"})
-        shadowRoot.appendChild(template.content.cloneNode(true))
-
-        this.startProgress()
-        this.determinePosition()
-      }
-
-      determinePosition() {
-        const elements = document.querySelectorAll("toast-message")
-
-        if (elements.length <= 1) return
-
-        const offset = elements[1].getBoundingClientRect()
-        const elementHeight = elements[1].offsetHeight
-        const screenHeight = document.documentElement.clientHeight
-
-        this.style.bottom = `calc(1rem + (${ screenHeight - offset.y }px))`
-      }
-
-      startProgress() {
-        const progressElement = this.shadowRoot.querySelector("[part='progress']")
-        progressElement.style.width = "100%"
-        setTimeout(() => { progressElement.style.width = 0 })
-        setTimeout(() => { this.removeElement() }, 3000)
-      }
-
-      removeElement() {
-        this.classList.add("is-fading-out")
-
-        setTimeout(() => {
-          this.remove()
-        }, 500)
-      }
-    }
-  )
+  return `${ ("0" + dateTime.getHours()).slice(-2) }:${ ("0" + dateTime.getMinutes()).slice(-2) }:${ ("0" + dateTime.getSeconds()).slice(-2) }`
 }
--- app/javascript/chat-item.js
+++ app/javascript/chat-item.js
@@ -1,9 +1,45 @@
 customElements.define("chat-item",
-  class extends HTMLElement {
+  class ChatItem extends HTMLElement {
     constructor() {
       super()
 
-      const template = document.getElementById("chat-item").content
-      const shadowRoot = this.attachShadow({mode: "open"}).appendChild(templateContent.cloneNode(true))
+      const template = document.getElementById("chat-item")
+      const shadowRoot = this.attachShadow({mode: "open"})
+      shadowRoot.appendChild(template.content.cloneNode(true))
+    }
+
+    connectedCallback() {
+      const usernameElement = this.shadowRoot.querySelector("[part='username']")
+      if (this.getAttribute("username")) {
+        usernameElement.innerText = this.getAttribute("username")
+        usernameElement.style.color = this.getAttribute("color")
+      } else {
+        usernameElement.remove()
+      }
+
+      const timestampElement = this.shadowRoot.querySelector("[part='timestamp']")
+      timestampElement.innerText = this.getAttribute("timestamp")
+
+      const timerElement = this.shadowRoot.querySelector("[part='timer']")
+      const time = document.querySelector("[data-role='chat-container']").dataset.expiration
+      time > 0 ? this.initiateCountdown(time, timerElement) : timerElement.remove()
+    }
+
+    initiateCountdown(time, element) {
+      element.style.animationDuration = time + "s"
+      element.style.background = `#606060`
+
+      let i = 0
+      const interval = setInterval(() => {
+        element.style.background = `conic-gradient(#606060 ${ 100 - (i * 2) }%, transparent 0%)`
+
+        i >= 50 ? clearInterval(interval) : i++
+      }, (time / 50) * 1000)
+
+      setTimeout(() => { this.classList.add("is-fading-out") }, time * 1000)
+      this.addEventListener("transitionend", () => {
+        this.remove()
+      })
+    }
   }
-})
+)
